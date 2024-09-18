2eb5d4e444d7cfc319f1839cf5959084455d342a
Manav-Chopra <852130@pdsb.net>
2024-09-18 14:24:59 -0400

    Create Circle.java

---
+++ Circle.java
@@ -0,0 +1,10 @@
+class Circle {
+  public static void main(String [] args) {
+    import java.lang.Math;
+      double circ;
+        double rad = 3.0; 
+          circ = 2 * Math.PI * rad; 
+            System.out.println(" The circumference of a circle of radius 3.0
+ * is " + circ);
+  }
+}
