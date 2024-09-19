a549ace5900c9ff26db9c61eeb3f7601d4576840
Manav-Chopra <852130@pdsb.net>
2024-09-19 12:49:02 -0400

    Create Math.java

---
+++ Math.java
@@ -0,0 +1,10 @@
+public class Math {
+    public static void main(String[] args) {
+        int amount = 8;
+        amount = amount + 1;
+        System.out.println(amount);
+        double a = 8;
+        int b = 3;
+        System.out.println(a/b);
+    }
+}
