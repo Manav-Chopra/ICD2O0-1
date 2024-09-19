96b262dc9778e3f35bcd81e948cb7035e7e66361
Manav-Chopra <852130@pdsb.net>
2024-09-19 12:38:27 -0400

    Create Cookies.java

---
+++ Cookies.java
@@ -0,0 +1,13 @@
+class Cookies {
+  public static void main(String[] args) {
+        System.out.println("Welcome to Sanjay's Koala Party!");
+        System.out.print("It's possible Sanjay can distribute " + 12);
+        System.out.println(" cookies for each bowl.");
+        System.out.println("Sanjay has 6 bowls for 144 cookies.");
+        double num;
+        num = 144/6;
+        System.out.println("The number of cookies per bowl is:" + num);
+        System.out.println("There are " + 28 + " people in this room.");
+       
+    }
+}
