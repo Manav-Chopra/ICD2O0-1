83e979f8c16d449bcf812c117d5bcf219df2cfdf
Manav-Chopra <852130@pdsb.net>
2024-09-19 12:09:21 -0400

    Update Circle.java

--- Circle.java
+++ Circle.java
@@ -1,10 +1,8 @@
 class Circle {
-  public static void main(String [] args) {
-    import java.lang.Math;
-      double circ;
-        double rad = 3.0; 
-          circ = 2 * Math.PI * rad; 
-            System.out.println(" The circumference of a circle of radius 3.0
- * is " + circ);
-  }
+      public static void main(String[] args) {
+        double circ;
+        double rad = 3;
+        circle = 2 * Math.PI * rad;
+        System.out.println("The circumference of a circle of radius " + rad + " is " + circle);
+      }
 }
