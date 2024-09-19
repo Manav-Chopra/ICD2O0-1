a40bde9141b5128f2fd1ca1c9a36e13561477791
Manav-Chopra <852130@pdsb.net>
2024-09-19 12:21:25 -0400

    Create Keyboard.java

---
+++ Keyboard.java
@@ -0,0 +1,6 @@
+class Keyboard {
+  public static void main(String[] args) {
+        int numberofkeys = 102;
+        System.out.println("The number of keys on a keyboard is" + numberofkeys);
+    }
+}
