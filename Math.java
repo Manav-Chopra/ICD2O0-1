dccd62ce06e3b3d59ab1a3196248a2e82d5c3026
Manav-Chopra <852130@pdsb.net>
2024-09-19 12:53:45 -0400

    Update Math.java

--- Math.java
+++ Math.java
@@ -1,10 +1,18 @@
-public class Math {
+public class Main {
     public static void main(String[] args) {
-        int amount = 8;
-        amount = amount + 1;
-        System.out.println(amount);
-        double a = 8;
+        int amount = 24;
+        System.out.println("Two dozen is: " + amount);
+        int amount1 = 7*8;
+        System.out.println("Forty two is: " + amount1);
+        int amount2 = 8;
+        amount2 = amount2 + 1;
+        System.out.println(amount2);
+        int a = 8;
         int b = 3;
+        System.out.println(a*b);
+        System.out.println(a/b);
+        double a1 = 8;
+        int b1 = 3;
         System.out.println(a/b);
     }
 }
